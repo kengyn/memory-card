3064f834ac4cfa005cea6f77408e3e471a23c4e1
Ken <knetngyn@gmail.com>
2022-09-16 18:44:40 -0500

    Add randomizer and refactor rendering of cards

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,12 +1,3 @@
-const RenderCard = ({ card }) => {
-  return (
-    <div className={"card " + card.element}>
-      <img src={card.src}></img>
-      <p>{card.name}</p>
-    </div>
-  );
-};
-
 const Card = () => {
   function importAll(r) {
     return r.keys().map(r);
@@ -15,36 +6,35 @@ const Card = () => {
   const images = importAll(require.context("../assets", false, /\.(webp)$/));
 
   const cards = [
-    { name: "Aether", src: images[0], element: "traveler" },
-    { name: "Amber", src: images[1], element: "pyro" },
-    { name: "Azhdaha", src: images[2], element: "geo" },
-    { name: "Fischl", src: images[3], element: "electro" },
-    { name: "Hu Tao", src: images[4], element: "pyro" },
-    { name: "Keqing", src: images[5], element: "electro" },
-    { name: "Lumine", src: images[6], element: "traveler" },
-    { name: "Mona", src: images[7], element: "hydro" },
-    { name: "Paimon", src: images[8], element: "traveler" },
-    { name: "Qiqi", src: images[9], element: "cryo" },
-    { name: "Rosaria", src: images[10], element: "cryo" },
-    { name: "Xiao", src: images[11], element: "anemo" },
+    { id: 0, name: "Aether", src: images[0], element: "traveler" },
+    { id: 1, name: "Amber", src: images[1], element: "pyro" },
+    { id: 2, name: "Azhdaha", src: images[2], element: "geo" },
+    { id: 3, name: "Fischl", src: images[3], element: "electro" },
+    { id: 4, name: "Hu Tao", src: images[4], element: "pyro" },
+    { id: 5, name: "Keqing", src: images[5], element: "electro" },
+    { id: 6, name: "Lumine", src: images[6], element: "traveler" },
+    { id: 7, name: "Mona", src: images[7], element: "hydro" },
+    { id: 8, name: "Paimon", src: images[8], element: "traveler" },
+    { id: 9, name: "Qiqi", src: images[9], element: "cryo" },
+    { id: 10, name: "Rosaria", src: images[10], element: "cryo" },
+    { id: 11, name: "Xiao", src: images[11], element: "anemo" },
   ];
 
-  return (
-    <div className="card-container">
-      <RenderCard card={cards[0]} />
-      <RenderCard card={cards[1]} />
-      <RenderCard card={cards[2]} />
-      <RenderCard card={cards[3]} />
-      <RenderCard card={cards[4]} />
-      <RenderCard card={cards[5]} />
-      <RenderCard card={cards[6]} />
-      <RenderCard card={cards[7]} />
-      <RenderCard card={cards[8]} />
-      <RenderCard card={cards[9]} />
-      <RenderCard card={cards[10]} />
-      <RenderCard card={cards[11]} />
-    </div>
-  );
+  const shuffled = cards
+    .map((val) => ({ val, sort: Math.random() }))
+    .sort((a, b) => a.sort - b.sort)
+    .map(({ val }) => val);
+
+  const renderCards = (deck) => {
+    return deck.map((card) => (
+      <div key={card.id} className={"card " + card.element}>
+        <img src={card.src}></img>
+        <p>{card.name}</p>
+      </div>
+    ));
+  };
+
+  return <div className="card-container">{renderCards(cards)}</div>;
 };
 
 export default Card;
